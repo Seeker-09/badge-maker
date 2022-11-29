397d1c746cb79337f0e52a1e263e93cac2a5f77e
Seeker <54086931+Seeker-09@users.noreply.github.com>
2022-11-29 10:45:26 -0500

    added the util class to clean up the code

--- CatWorx.BadgeMaker/Program.cs
+++ CatWorx.BadgeMaker/Program.cs
@@ -4,7 +4,7 @@ namespace CatWorx.BadgeMaker {
   class Program {
     static void Main(string[] args) {
       List<Employee> employees = GetEmployees();
-      PrintEmployees(employees);
+      Util.PrintEmployees(employees);
     }
 
     static List<Employee> GetEmployees() {
@@ -29,13 +29,6 @@ namespace CatWorx.BadgeMaker {
       }
 
       return employees;
-    }
-
-    static void PrintEmployees(List<Employee> employees) {
-      for(int i = 0; i < employees.Count; i++) {
-        string template = "{0,-10}\t{1,-20}\t{2}";
-        Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));
-      }
     }
   }
 }
